e579192d8a200cf57c63ac79f9662fc94eac647b
ryan_PC <wm25-ryantys@student.tarc.edu.my>
2026-03-30 16:34:26 +0800

    Added save to file, continnue load to file later

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
 
 import Vehicles.*;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         InventoryManagement Inventory = new InventoryManagement();
         Inventory.addCar(new SUV("Ativa","A033",10,true,5,true,false));
         Inventory.addCar(new Economy("Ativa","A032",10,true,5,5));
-        Inventory.addCar(new Luxury("Ativa","A031",10,true,5,true));
         Inventory.addCar(new SUV("Ativa","A030",10,true,5,true,false));
-
+        Inventory.addCar(new Luxury("Ativa","A031",10,true,5,true));
+        Inventory.loadfromFile();
         System.out.println("This is me trying to connect from PC");
         System.out.println("testing out branches!");
     }
 }
+
+//
--- src/Vehicles/InventoryManagement.java
+++ src/Vehicles/InventoryManagement.java
@@ -5,20 +5,24 @@ import java.io.*;
 
 public class InventoryManagement {
     private ArrayList<Car> Inventory;
+    private final String dataFile = "inventory.dat";
+    private static int count = 0;
 
-    public InventoryManagement (){
+    public InventoryManagement() throws IOException {
         Inventory = new ArrayList<>();
+        loadfromFile();
 
     }
 
     public void addCar(Car car){
         Inventory.add(car);
         savetoFile(Inventory);
+        count++;
     }
 
-    public void savetoFile(ArrayList<Car> Inventory){
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("inventory.dat"))) {
-            for (Car car:Inventory){
+    public void savetoFile(ArrayList<Car> Inventory) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(dataFile))) {
+            for (Car car : Inventory) {
                 oos.writeObject(car);
             }
         } catch (IOException e) {
@@ -26,13 +30,22 @@ public class InventoryManagement {
         }
 
     }
+    public void loadfromFile() throws IOException {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(dataFile))){
+            for (int i = 0; i<getCount();i++){
+                System.out.println(ois.readObject());
+                Inventory.add((Car)ois.readObject());
+            }
 
-    public void loadFromFile(ArrayList<Car> Inventory) {
-  //      try (ObjectInputStream oos = new ObjectInputStream(new FileInputStream("inventory.dat"))) {
-
-     //   } catch (IOException e) {
-     //       throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e){
+            System.out.println("Error");
         }
     }
+    public static int getCount(){
+        return count;
+    }
+}
 
 
